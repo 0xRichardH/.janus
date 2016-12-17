688accaad6785720db230422214febeb8d4afea1
haoxilu <ithaoxilu@gmail.com>
2016-12-17 10:04:46 +0800

    add some plugin

--- install.sh
+++ install.sh
@@ -13,3 +13,7 @@ for rc in "vimrc.after" "vimrc.before" "gvimrc.after" "gvimrc.before" "ackrc"; d
   ln -s "${HOME}/.janus/rcs/${rc}" "${HOME}/.${rc}" || die "Could not link ${rc} file to .${rc}"
   echo ".${rc} has been linked to .janus ${rc}"
 done
+
+# Add fonts to system
+open "${HOME}/.janus/fonts/Meslo/*"
+echo "Please add fonts to System and change Iterm Fonts Settings"
